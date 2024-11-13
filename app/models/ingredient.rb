9a283b1e0a7ff1a056539e0a3650345dbdebe9c4
AnabalonJT <jtanabalon@miuandes.cl>
2024-11-13 19:35:34 -0300

    seed done por ingredients,recipes and joinn table

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,6 @@
 class Ingredient < ApplicationRecord
+  enum measure: { gr: 0, ml: 1, un: 2 }
+  
   has_many :recipe_ingredients
   has_many :recipes, through: :recipe_ingredients
   has_many :forbidden_food_users
