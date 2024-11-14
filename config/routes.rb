311791e3e1c257fb101f8e008044a590a4a748ac
AnabalonJT <jtanabalon@miuandes.cl>
2024-11-14 14:43:40 -0300

    fix devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,27 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  # Set the root to the Devise login page
-  authenticated :user do
-    root 'trips#index', as: :authenticated_root
-  end
+  devise_scope :user do
+    authenticated :user do
+      root 'trips#index', as: :authenticated_root
+    end
 
-  unauthenticated do
-    root 'devise/sessions#new', as: :unauthenticated_root
+    unauthenticated do
+      root 'devise/sessions#new', as: :unauthenticated_root
+    end
   end
-
+  
   # Other routes, such as resources
   resources :trips do
     post 'add_recipe', to: 'trips#add_recipe', as: :add_recipe, on: :member
   end
-  
   resources :recipes do
-    get "join_trip" , to: 'trips#join_trip', as: :join_trip, on: :member
+    get "join_trip", to: 'trips#join_trip', as: :join_trip, on: :member
   end
 
-  get "up" => "rails/health#show", as: :rails_health_check
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
 end
